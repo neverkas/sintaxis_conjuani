ad88855f399b9bdf15bdd13f90314788668e1a70
Emmanuel <emmanuel.lazarte.012@gmail.com>
2020-08-23 22:30:14 -0300

    - Primer ejercicio

---
+++ c05Ejercicios1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+unsigned longitudCadena (const char* cadena)
+{
+  /*
+    EJERCICIO 1: Completá la definición de esta función para que devuelva la cantidad de caracteres de cadena 
+    (parámetro de entrada de la función), así
+    longitudCadena ("Hola") deberá devolver 4
+    longitudCadena ("") deberá devolver 0
+    longitudCadena ("Titulo\t1") deberá devolver 8
+
+    REQUISITOS: 
+    * Se debe usar la palabra reservada: for
+    * La función deberá comportarse en forma análoga a unsigned strlen (const char*) declarada en string.h
+    */
+  return (0);
+}
