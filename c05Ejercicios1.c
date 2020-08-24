42458d0ce2b58e1117b65b941850b39359828f2e
Emmanuel <emmanuel.lazarte.012@gmail.com>
2020-08-24 00:54:23 -0300

    Primer ejercicio

--- c05Ejercicios1.c
+++ c05Ejercicios1.c
@@ -1,17 +1,35 @@
 #include <stdio.h>
 
-unsigned longitudCadena (const char* cadena)
+unsigned longitud_Cadena (const char* cadena)
 {
-  /*
-    EJERCICIO 1: Completá la definición de esta función para que devuelva la cantidad de caracteres de cadena 
-    (parámetro de entrada de la función), así
-    longitudCadena ("Hola") deberá devolver 4
-    longitudCadena ("") deberá devolver 0
-    longitudCadena ("Titulo\t1") deberá devolver 8
+  int i;
 
-    REQUISITOS: 
-    * Se debe usar la palabra reservada: for
-    * La función deberá comportarse en forma análoga a unsigned strlen (const char*) declarada en string.h
-    */
-  return (0);
+  for(i = 0; *(cadena+i) != '\0'; i++)
+    ;
+  return i;
 }
+
+int main(){
+  int a;
+  //char cadena[50];
+  char* cadena;
+  printf("Digite su cadena: ");
+  scanf("%s", cadena);
+  a = longitud_Cadena(cadena);
+  printf("\n\nLa cadena tiene longitud: %d \n\n", a);
+
+  return 0;
+}
+/*
+  EJERCICIO 1:
+  Completá la definición de esta función para que devuelva la cantidad de caracteres de cadena
+  (parámetro de entrada de la función), así
+  longitudCadena ("Hola") deberá devolver 4
+  longitudCadena ("") deberá devolver 0
+  longitudCadena ("Titulo\t1") deberá devolver 8
+
+  REQUISITOS:
+  * Se debe usar la palabra reservada: for
+  * La función deberá comportarse en forma análoga a unsigned strlen (const char*) declarada en string.h
+  */
+    
